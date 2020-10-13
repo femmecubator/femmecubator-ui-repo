3b0f38469b5c8db85c62090c2afc40aa0f6868c4
cfernando415 <ca.fernando415@gmail.com>
2020-10-12 21:49:36 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div
+        >
+          Femmecubator - Coming Soon!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
