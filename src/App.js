2f35425fa4c3b9971587139542a69613db38f485
cfernando415 <ca.fernando415@gmail.com>
2020-10-15 22:35:31 -0400

    created mock server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { mockServer } from "./mock/mockServer";
+
+if (process.env.REACT_APP_MOCK_API_TRUE) {
+  console.log("starting mock server...");
+  mockServer();
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div
-        >
-          Femmecubator - Coming Soon!
-        </div>
+        <div>Femmecubator - Coming Soon!</div>
       </header>
     </div>
   );
---
+++ src/mock/data/index.js
@@ -0,0 +1,16 @@
+const commonMenuData = {
+  headers: [
+    {
+      id: 1,
+      label: "About",
+      href: "/about",
+    },
+    {
+      id: 2,
+      label: "Join Today",
+      href: "/registration",
+    },
+  ],
+};
+
+export { commonMenuData };
