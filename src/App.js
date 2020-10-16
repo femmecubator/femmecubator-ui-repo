18738dcad4c9a58baf511849fe05a592545e30b8
vuonga1103 <vuonga1103@yahoo.com>
2020-10-16 10:09:28 -0400

    basic header skeleton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { mockServer } from "./mock/mockServer";
+import Header from "./Header/Header";
 
 if (process.env.REACT_APP_MOCK_API_TRUE) {
   console.log("starting mock server...");
@@ -11,10 +12,7 @@ if (process.env.REACT_APP_MOCK_API_TRUE) {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>Femmecubator - Coming Soon!</div>
-      </header>
+      <Header/>
     </div>
   );
 }
