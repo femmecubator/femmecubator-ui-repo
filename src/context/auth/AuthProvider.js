721e01f2afd506372db2b5ebb482b0cbb2cd0e7c
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2020-10-16 21:11:23 -0400

    added auth provider (#11)
    * added auth provider
    
    * removed console log

---
+++ src/context/auth/AuthProvider.js
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from "react";
+import Auth from "./auth";
+
+const AuthContext = createContext();
+
+function AuthProvider(props) {
+  const auth = new Auth();
+  return <AuthContext.Provider value={{ auth }} {...props} />;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
---
+++ src/utils/cookies.js
@@ -0,0 +1,11 @@
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+function isAuthCookiesExists() {
+  return !!(
+    cookies.get("FemmecubatorSession") || process.env.NODE_ENV !== "production"
+  );
+}
+
+export { isAuthCookiesExists };
