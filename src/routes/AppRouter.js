02f4df14d128ea13f473d8655d8f2046279b0e7a
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2020-10-20 20:21:08 -0400

    Update AppRouter.js
    converted arrow function to regular expression

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,20 +4,17 @@ import ProtectedRoute from './ProtectedRoute';
 import { routes } from '../utils/constants';
 
 const AppRouter = () => {
-  const renderRoutes = () => {
-    console.log(routes);
-    return routes.map(({ path, component, isProtected }, key) =>
-      isProtected ? (
-        <ProtectedRoute exact path={path} component={component} key={key} />
-      ) : (
-        <Route exact path={path} component={component} key={key} />
-      )
-    );
-  };
+  const renderRoutes = routes.map(({ path, component, isProtected }, key) =>
+    isProtected ? (
+      <ProtectedRoute exact path={path} component={component} key={key} />
+    ) : (
+      <Route exact path={path} component={component} key={key} />
+    )
+  );
 
   return (
     <div>
-      <Switch>{renderRoutes()}</Switch>
+      <Switch>{renderRoutes}</Switch>
     </div>
   );
 };
