a4ace7a77d3ba35f7243b57858c13f9d6f2b516f
Anh <58869812+vuonga1103@users.noreply.github.com>
2020-11-02 21:42:57 -0500

    changed header color (#25)
    * changed header color
    
    * removed a comment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles(() => ({
     flexGrow: 1,
   },
   header: {
-    backgroundColor: '#3E50B4',
+    backgroundColor: '#400CCC',
     '@media (min-width: 799px)': {
       paddingRight: '79px',
       paddingLeft: '118px',
@@ -64,6 +64,7 @@ const useStyles = makeStyles(() => ({
     fontFamily: 'Work Sans, sans-serif',
     fontWeight: 600,
     size: '24px',
+    color: '#FFFEFE',
   },
 }));
 
@@ -137,7 +138,7 @@ export default function Header() {
       return menuHeaders
         .filter(({ href }) => href !== '/logout' && href !== '/account')
         .map(({ id, href, label }) => {
-          let color = label === 'Join Us!' ? '#50E3C2' : 'white';
+          let color = label === 'Join Us!' ? 'white' : 'white';
 
           return (
             <Button
