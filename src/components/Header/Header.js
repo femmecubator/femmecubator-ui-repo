77f01c880d2d5b91af9fe746a7fa5a7258200c72
Anh <58869812+vuonga1103@users.noreply.github.com>
2020-11-04 09:24:35 -0500

    cleaned up import statements (#32)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import {
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Drawer,
+  Link,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import axios from 'axios';
 import { API_PATH, DEFAULT_COMMON_MENU } from '../../utils/constants';
 import { useAuth } from '../../context/auth';
-import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
-import { Drawer } from '@material-ui/core';
 import _ from 'lodash';
 
 const useStyles = makeStyles(() => ({
