f379aeb9e38d94d868024faf421bf09b6fe28c16
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2020-11-09 22:14:22 -0500

    moved isMobile to global provider and fixed navigation defect (#36)
    * moved isMobile to global provider and fixed navigation defect
    
    * removed commented code
    
    * removed console.log
    
    * updated environment variable name in testing
    
    * fixed data mapping issue
    
    * removed console.log
    
    * hide nav items in various targeted path names
    
    * fixed unit test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { initialize, pageview } from 'react-ga';
 import './App.css';
 import { mockServer } from './mock/mockServer';
 import { TRACKING_ID } from './utils/constants';
 import Header from './components/Header/Header';
 import AppRouter from 'routes/AppRouter';
+import { GlobalContext } from 'context/global';
+import { updateView } from 'context/actionCreators';
 
 if (process.env.REACT_APP_MOCK_API_TRUE) {
-  console.log('starting mock server...');
   mockServer();
 }
 
+const setResponsiveness = () => {
+  return window.innerWidth < 799;
+};
+
 function App() {
+  const { dispatch } = useContext(GlobalContext);
+
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
       initialize(TRACKING_ID);
-      // track page views and language location
       pageview(window.location.pathname + window.location.search);
     }
-  }, []);
+    window.addEventListener('resize', () =>
+      dispatch(updateView(setResponsiveness()))
+    );
+    return () =>
+      window.removeEventListener(
+        'resize',
+        dispatch(updateView(setResponsiveness()))
+      );
+  }, [dispatch]);
 
   return (
     <div className="App">
--- src/context/auth/AuthProvider.js
+++ src/context/auth/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from 'react';
-import Auth from './auth';
+import Auth from 'utils/auth';
 
 const AuthContext = createContext();
 
