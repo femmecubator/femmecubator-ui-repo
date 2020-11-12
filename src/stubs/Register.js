126067273be23172643befed6bf2aca820f977b6
cfernando415 <ca.fernando415@gmail.com>
2020-11-12 10:41:38 -0500

    throw error for test

--- src/stubs/Register.js
+++ src/stubs/Register.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Register = () => {
+  useEffect(() => {
+    throw new Error('this is a test to check if Error boundary is working');
+  }, []);
   return <div>Register</div>;
 };
 
