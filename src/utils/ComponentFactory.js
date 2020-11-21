44dbcb69274f9219bb47b550c01655310bdc95a9
Carlo <ca.fernando415@gmail.com>
2020-11-21 13:41:14 -0500

    fixed ComponentFactory

--- src/utils/ComponentFactory.js
+++ src/utils/ComponentFactory.js
@@ -1,20 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { Landing, Login, Register, Mentors, Listings, Volunteer } from 'stubs';
+import { Login, Register, Mentors, Listings, Volunteer } from 'stubs';
+import Home from 'components/Home/Home';
 import { ROUTES_LABEL } from 'utils/constants';
-const {
-  LANDING_PAGE,
-  MENTORS,
-  LOGIN,
-  REGISTER,
-  LISTINGS,
-  VOLUNTEER,
-} = ROUTES_LABEL;
+const { HOME, MENTORS, LOGIN, REGISTER, LISTINGS, VOLUNTEER } = ROUTES_LABEL;
 
 export default class ComponentFactory {
   static create(label) {
     switch (label) {
-      case LANDING_PAGE:
-        return Landing;
+      case HOME:
+        return Home;
       case MENTORS:
         return Mentors;
       case LOGIN:
