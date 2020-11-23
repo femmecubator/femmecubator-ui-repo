6acd9a722f706622f4f922c5bc2d2d0895ded09c
Carlo <ca.fernando415@gmail.com>
2020-11-23 00:07:08 -0500

    registration form

--- src/components/AccountForms/RegistrationForm/index.js
+++ src/components/AccountForms/RegistrationForm/index.js
@@ -1,56 +1,86 @@
 import React from 'react';
-import KeyboardIcon from '@material-ui/icons/Keyboard';
+import SchoolIcon from '@material-ui/icons/School';
 import './registration.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    width: '75%',
-    margin: '0 auto',
-    float: 'none',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
   },
-  details: {
-    display: 'flex',
-    flexDirection: 'column',
+  paperContainer: {
+    position: 'absolute',
+    width: 'auto',
+    height: 'auto',
   },
-  content: {
-    flex: '1 0 auto',
+  bookMentor: {
+    fontFamily: 'Work Sans, sans-serif',
+    fontSize: '28px',
+    fontWeight: 700,
+    paddingTop: '20px',
   },
-  cover: {
-    width: 151,
-  },
-  controls: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
-  playIcon: {
-    height: 38,
-    width: 38,
+  bookMentorDesc: {
+    paddingLeft: '35px',
+    paddingRight: '35px',
+    fontFamily: 'Open Sans, sans-serif',
+    fontWeight: 400,
+    fontSize: '16px',
+    lineHeight: '24px',
+    paddingTop: '20px',
   },
 }));
-
+const FORM_TITLE = 'Create an account';
+const FORM_SUBTITLE = 'Have an existing account?';
 const RegistrationForm = () => {
   const classes = useStyles();
 
   return (
     <>
-      <div className="registration-form-container">
-        <Card className={classes.root}>
-          <div className={classes.details}>
-            <CardContent className={classes.content}></CardContent>
-            <div className={classes.controls}></div>
-          </div>
-          <div className="circle">
-            <div className="center">
-              <KeyboardIcon style={{ height: '56.11px', width: '39.28px' }} />
+      <div className={classes.root}>
+        <Paper className={classes.paperContainer}>
+          <div className="registration-form-container">
+            <div className="registration-form">
+              <div>{FORM_TITLE}</div>
+              <div>{FORM_SUBTITLE}</div>
+            </div>
+            <div className="vl">
+              <div style={{ top: '25%', position: 'relative' }}>
+                <div className="circle">
+                  <div className="center">
+                    <SchoolIcon
+                      style={{
+                        height: '56.11px',
+                        width: '39.28px',
+                        color: '#550CCC',
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <Typography variant="h2" className={classes.bookMentor}>
+                    Book Mentors G
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    className={classes.bookMentorDesc}
+                  >
+                    Are you thinking of going into Design or Development career
+                    tracks? Easily book time with mentors who can help with
+                    portfolio reviews, practice interview and whiteboarding
+                    sessions.
+                  </Typography>
+                </div>
+              </div>
             </div>
           </div>
-        </Card>
+        </Paper>
       </div>
     </>
   );
