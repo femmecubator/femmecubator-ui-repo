844c6b284ee3cf6d011176855fb1dac2339ea6b0
Carlo <ca.fernando415@gmail.com>
2020-11-29 20:33:47 -0500

    added husky eslint prettier and removed throw error in stub component

--- src/components/ErrorHandling/ErrorFallback.test.js
+++ src/components/ErrorHandling/ErrorFallback.test.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { render, cleanup, fireEvent, getByText } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import ErrorFallback from './ErrorFallback';
 
 describe('<ErrorFallback />', () => {
-    afterEach(cleanup)
-    it('renders correct error image', () => {
-        const { getByTestId } = render(<ErrorFallback />);
-        const errorImg = getByTestId('errorIllustration').getAttribute('src');
-        expect(errorImg).toBe('errorIllustration.svg');
-    });
+  afterEach(cleanup);
+  it('renders correct error image', () => {
+    const { getByTestId } = render(<ErrorFallback />);
+    const errorImg = getByTestId('errorIllustration').getAttribute('src');
+    expect(errorImg).toBe('errorIllustration.svg');
+  });
 
-    it('renders error text', () => {
-        const { getByRole } = render(<ErrorFallback />);
-        const errorTitle = getByRole('heading');
-        expect(errorTitle).toHaveTextContent(`We're having trouble loading this page.`)
+  it('renders error text', () => {
+    const { getByRole } = render(<ErrorFallback />);
+    const errorTitle = getByRole('heading');
+    expect(errorTitle).toHaveTextContent(
+      `We're having trouble loading this page.`
+    );
+  });
 
-    })
-
-    it('it reloads page on click', () => {
-        const { location } = window;
-        delete window.location;
-        window.location = { reload: jest.fn() };
-        expect(window.location.reload).not.toHaveBeenCalled();
-        const { getByTestId, container, debug } = render(<ErrorFallback onClick={window.location.reload} />);
-        const reloadButton = getByTestId('reloadButton');
-        fireEvent.click(reloadButton);
-        expect(window.location.reload).toHaveBeenCalledTimes(1);
-    })
-})
+  it('it reloads page on click', () => {
+    delete window.location;
+    window.location = { reload: jest.fn() };
+    expect(window.location.reload).not.toHaveBeenCalled();
+    const { getByTestId } = render(
+      <ErrorFallback onClick={window.location.reload} />
+    );
+    const reloadButton = getByTestId('reloadButton');
+    fireEvent.click(reloadButton);
+    expect(window.location.reload).toHaveBeenCalledTimes(1);
+  });
+});
