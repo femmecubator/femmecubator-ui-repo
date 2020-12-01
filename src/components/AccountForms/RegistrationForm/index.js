ef8b7f0e59a99ea52872fa4531ad3c456ea66154
Carlo <ca.fernando415@gmail.com>
2020-12-01 00:32:16 -0500

    mobile view

--- src/components/AccountForms/RegistrationForm/index.js
+++ src/components/AccountForms/RegistrationForm/index.js
@@ -20,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     marginTop: '5%',
   },
+  rootMobile: {
+    justifyContent: 'center',
+  },
   paperContainer: {
     position: 'absolute',
     width: 'auto',
@@ -79,7 +82,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const FORM_TITLE = 'Create an account';
+const FORM_TITLE = 'Create account';
 const FORM_SUBTITLE = 'Have an existing account?';
 
 const RegistrationForm = () => {
@@ -94,160 +97,191 @@ const RegistrationForm = () => {
     alert(JSON.stringify(data));
   };
   let content = (
-    <Paper className={classes.paperContainer}>
-      <Grid container>
-        <Grid item>
-          <div className="registration-form-container">
-            <div className="registration-form">
-              <Typography variant="h2" className={classes.formTitle}>
-                {FORM_TITLE}
-              </Typography>
-              <Typography variant="body2" className={classes.formSubtitle}>
-                {FORM_SUBTITLE} <Link to="/login">Login</Link>
-              </Typography>
-              <form noValidate onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  <TextField
-                    {...{
-                      label: 'First Name',
-                      variant: 'outlined',
-                      name: 'firstName',
-                      inputRef: register({ required: true }),
-                      error: errors.firstName,
-                      helperText: errors.firstName && 'First name is required',
-                    }}
-                  />
-                  <TextField
-                    {...{
-                      inputRef: register({ required: true }),
-                      name: 'lastName',
-                      label: 'Last Name',
-                      variant: 'outlined',
-                      className: classes.textFieldSpacing,
-                      error: errors.lastName,
-                      helperText: errors.lastName && 'Last name is required',
-                    }}
-                  />
-                </div>
-                <div className={classes.inputSpacing}>
-                  <TextField
-                    {...{
-                      label: 'Preferred Location',
-                      variant: 'outlined',
-                      inputRef: register({ required: true }),
-                      name: 'preferredLocation',
-                      error: errors.preferredLocation,
-                      helperText:
-                        errors.preferredLocation &&
-                        'Preferred Location is required',
-                    }}
-                  />
-                </div>
-                <div className={classes.inputSpacing}>
-                  <TextField
-                    {...{
-                      label: 'Email',
-                      variant: 'outlined',
-                      inputRef: register({
-                        required: 'Email is required',
-                        pattern: {
-                          value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                          message: 'Invalid email format',
-                        },
-                      }),
-                      name: 'email',
-                      error: errors.email,
-                      helperText: errors.email && errors.email.message,
-                    }}
-                  />
-                </div>
-                <div className={classes.inputSpacing}>
-                  <TextField
-                    {...{
-                      label: 'Username',
-                      variant: 'outlined',
-                      inputRef: register({ required: true }),
-                      name: 'userName',
-                      error: errors.userName,
-                      helperText: errors.userName && 'Username is required',
-                    }}
-                  />
-                </div>
-                <div className={classes.inputSpacing}>
-                  <TextField
-                    {...{
-                      label: 'Password',
-                      variant: 'outlined',
-                      inputRef: register,
-                      name: 'password',
-                      type: 'password',
-                    }}
-                  />
-                  <TextField
-                    {...{
-                      label: 'Retype Password',
-                      variant: 'outlined',
-                      className: classes.textFieldSpacing,
-                      inputRef: register,
-                      name: 'retypePassword',
-                      type: 'password',
-                    }}
-                  />
-                </div>
-                <div>
-                  <Button
-                    {...{
-                      type: 'submit',
-                      variant: 'contained',
-                      color: 'primary',
-                      className: classes.button,
-                    }}
-                  >
-                    Submit
-                  </Button>
-                </div>
-              </form>
-            </div>
-            <div className="vl">
-              <div style={{ top: '15%', position: 'relative' }}>
-                <div className="circle">
-                  <div className="center">
-                    <SchoolIcon className={classes.schoolIcon} />
+    <div className={classes.root}>
+      <Paper className={classes.paperContainer}>
+        <Grid container>
+          <Grid item xs={12}>
+            <div className="registration-form-container">
+              <div className="registration-form">
+                <Typography variant="h2" className={classes.formTitle}>
+                  {FORM_TITLE}
+                </Typography>
+                <Typography variant="body2" className={classes.formSubtitle}>
+                  {FORM_SUBTITLE} <Link to="/login">Login</Link>
+                </Typography>
+                <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                  <div>
+                    <TextField
+                      {...{
+                        label: 'First Name',
+                        variant: 'outlined',
+                        name: 'firstName',
+                        inputRef: register({ required: true }),
+                        error: errors.firstName,
+                        helperText:
+                          errors.firstName && 'First name is required',
+                      }}
+                    />
+                    <TextField
+                      {...{
+                        inputRef: register({ required: true }),
+                        name: 'lastName',
+                        label: 'Last Name',
+                        variant: 'outlined',
+                        className: classes.textFieldSpacing,
+                        error: errors.lastName,
+                        helperText: errors.lastName && 'Last name is required',
+                      }}
+                    />
                   </div>
-                </div>
-                <div>
-                  <Typography variant="h2" className={classes.bookMentor}>
-                    Book Mentors
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    className={classes.bookMentorDesc}
-                  >
-                    Are you thinking of going into Design or Development career
-                    tracks? Easily book time with mentors who can help with
-                    portfolio reviews, practice interview and whiteboarding
-                    sessions.
-                  </Typography>
+                  <div className={classes.inputSpacing}>
+                    <TextField
+                      {...{
+                        label: 'Preferred Location',
+                        variant: 'outlined',
+                        inputRef: register({ required: true }),
+                        name: 'preferredLocation',
+                        error: errors.preferredLocation,
+                        helperText:
+                          errors.preferredLocation &&
+                          'Preferred Location is required',
+                      }}
+                    />
+                  </div>
+                  <div className={classes.inputSpacing}>
+                    <TextField
+                      {...{
+                        label: 'Email',
+                        variant: 'outlined',
+                        inputRef: register({
+                          required: 'Email is required',
+                          pattern: {
+                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                            message: 'Invalid email format',
+                          },
+                        }),
+                        name: 'email',
+                        error: errors.email,
+                        helperText: errors.email && errors.email.message,
+                      }}
+                    />
+                  </div>
+                  <div className={classes.inputSpacing}>
+                    <TextField
+                      {...{
+                        label: 'Username',
+                        variant: 'outlined',
+                        inputRef: register({ required: true }),
+                        name: 'userName',
+                        error: errors.userName,
+                        helperText: errors.userName && 'Username is required',
+                      }}
+                    />
+                  </div>
+                  <div className={classes.inputSpacing}>
+                    <TextField
+                      {...{
+                        label: 'Password',
+                        variant: 'outlined',
+                        inputRef: register,
+                        name: 'password',
+                        type: 'password',
+                      }}
+                    />
+                    <TextField
+                      {...{
+                        label: 'Retype Password',
+                        variant: 'outlined',
+                        className: classes.textFieldSpacing,
+                        inputRef: register,
+                        name: 'retypePassword',
+                        type: 'password',
+                      }}
+                    />
+                  </div>
+                  <div>
+                    <Button
+                      {...{
+                        type: 'submit',
+                        variant: 'contained',
+                        color: 'primary',
+                        className: classes.button,
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                </form>
+              </div>
+              <div className="vl">
+                <div style={{ top: '15%', position: 'relative' }}>
+                  <div className="circle">
+                    <div className="center">
+                      <SchoolIcon className={classes.schoolIcon} />
+                    </div>
+                  </div>
+                  <div>
+                    <Typography variant="h2" className={classes.bookMentor}>
+                      Book Mentors
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      className={classes.bookMentorDesc}
+                    >
+                      Are you thinking of going into Design or Development
+                      career tracks? Easily book time with mentors who can help
+                      with portfolio reviews, practice interview and
+                      whiteboarding sessions.
+                    </Typography>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </div>
   );
 
-  return (
-    <>
-      <div className={classes.root}>{content}</div>
-      {/* <CssBaseline />
-      <Grid container direction="row" alignItems="center" justify="center">
-        <Grid item xs={12}>
-          {content}
+  if (isMobile) {
+    content = (
+      <div className={classes.rootMobile}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          spacing={0}
+        >
+          <Grid item xs={12}>
+            <Typography variant="h2" className={classes.formTitle} nowrap>
+              {FORM_TITLE}
+            </Typography>
+            <Typography variant="body2" className={classes.formSubtitle}>
+              {FORM_SUBTITLE} <Link to="/login">Login</Link>
+            </Typography>
+            <Grid item xs={12} style={{ paddingLeft: 0, paddingRight: 0 }}>
+              <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                <TextField
+                  {...{
+                    label: 'First Name',
+                    variant: 'outlined',
+                    name: 'firstName',
+                    inputRef: register({ required: true }),
+                    error: errors.firstName,
+                    helperText: errors.firstName && 'First name is required',
+                    fullWidth: true,
+                  }}
+                />
+              </form>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid> */}
-    </>
-  );
+      </div>
+    );
+  }
+
+  return <>{content}</>;
 };
 
 export default RegistrationForm;
