5bf29dc67a8b63aebd870323caf971700b761058
PeaWarrior <jacksonchen171@gmail.com>
2020-12-01 12:44:30 -0500

    initiated LoginForm component

---
+++ src/components/AccountForms/LoginForm/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const LoginForm = () => {
+  
+  return (
+    <>
+      <div>
+        Login Form
+      </div>
+    </>
+  )
+};
+
+export default LoginForm;
