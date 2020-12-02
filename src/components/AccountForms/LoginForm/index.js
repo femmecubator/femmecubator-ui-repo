f3a228b74aeada57322ea5f61f297e575574c2a1
PeaWarrior <jacksonchen171@gmail.com>
2020-12-02 00:39:09 -0500

    added TextFields for email and password

--- src/components/AccountForms/LoginForm/index.js
+++ src/components/AccountForms/LoginForm/index.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import { TextField, InputAdornment, IconButton } from '@material-ui/core';
+import { Visibility, VisiblityOff } from '@material-ui/icons';
+
+const FORM_TITLE = 'Welcome back!'
+
 const LoginForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+
+  const emailRequirements = {
+    required: true,
+    pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  };
+
+  const passwordRequirements = {
+    required: true,
+    pattern: /(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])/,
+  }
   
   return (
     <>
-      <div>
-        Login Form
-      </div>
+      {(errors.email || errors.password) && <p>Sorry, invalid email or password.</p>}
+      <h2>{FORM_TITLE}</h2>
+      <form onSubmit={handleSubmit(onSubmit)} >
+        <TextField 
+          name='Email'
+          label='Email'
+          inputRef={register(emailRequirements)}
+          variant='outlined'
+        />
+
+        <TextField 
+          name='Password'
+          label='Password'
+          variant='outlined'
+          type='password'
+          inputRef={register(passwordRequirements)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <IconButton
+                  aria-label='toggle password visibility'
+                >
+                  <Visibility />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <input type="submit" value='SIGN IN' />
+      </form>
     </>
   )
 };
