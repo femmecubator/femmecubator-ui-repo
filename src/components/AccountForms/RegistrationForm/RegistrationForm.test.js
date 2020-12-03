979fccfcaf55e54bca8425afa2a8c6fdbd06770b
cfernando415 <ca.fernando415@gmail.com>
2020-12-03 14:57:06 -0500

    added todos for reg form unit test

---
+++ src/components/AccountForms/RegistrationForm/RegistrationForm.test.js
@@ -0,0 +1,9 @@
+import RegistrationForm from './index';
+
+describe('<RegistrationForm />', () => {
+  it.todo('should check if the submit button handler has been called');
+
+  it.todo('should validate errors for required fields');
+
+  it.todo('should validate error for fields with invalid format');
+});
