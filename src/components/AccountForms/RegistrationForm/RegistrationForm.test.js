94ebeacb44ac267977cfcff8d9751bbbf34ea38c
cfernando415 <ca.fernando415@gmail.com>
2020-12-04 00:46:34 -0500

    unit test done

--- src/components/AccountForms/RegistrationForm/RegistrationForm.test.js
+++ src/components/AccountForms/RegistrationForm/RegistrationForm.test.js
@@ -1,9 +1,119 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import RegistrationForm from './index';
+import { act } from 'react-dom/test-utils';
+import { GlobalProvider } from 'context/global';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('<RegistrationForm />', () => {
-  it.todo('should check if the submit button handler has been called');
+  it('should check if the submit button handler has been called', async () => {
+    const mockOnSubmit = jest.fn();
 
-  it.todo('should validate errors for required fields');
+    const { getByTestId } = render(
+      <GlobalProvider>
+        <Router>
+          <RegistrationForm onSubmit={mockOnSubmit} />
+        </Router>
+      </GlobalProvider>
+    );
+    await act(async () => {
+      fireEvent.input(getByTestId('firstName'), { target: { value: 'John' } });
+      fireEvent.input(getByTestId('lastName'), { target: { value: 'Doe' } });
+      fireEvent.input(getByTestId('prefLoc'), { target: { value: 'NY' } });
+      fireEvent.input(getByTestId('title'), {
+        target: { value: 'Software Engineer' },
+      });
+      fireEvent.input(getByTestId('email'), {
+        target: { value: 'johndoe@gmail.com' },
+      });
+      fireEvent.input(getByTestId('userName'), {
+        target: { value: 'john.doe' },
+      });
+      fireEvent.input(getByTestId('password'), {
+        target: { value: 'JDoe12345!' },
+      });
+      fireEvent.input(getByTestId('retypePassword'), {
+        target: { value: 'JDoe12345!' },
+      });
+    });
 
-  it.todo('should validate error for fields with invalid format');
+    await act(async () => {
+      fireEvent.submit(getByTestId('submit'));
+    });
+    expect(mockOnSubmit).toHaveBeenCalled();
+  });
+
+  it('should validate errors for required fields', async () => {
+    const mockOnSubmit = jest.fn();
+    const { getByTestId, container } = render(
+      <GlobalProvider>
+        <Router>
+          <RegistrationForm onSubmit={mockOnSubmit} />
+        </Router>
+      </GlobalProvider>
+    );
+
+    await act(async () => {
+      fireEvent.submit(getByTestId('submit'));
+    });
+    const passwordError = await screen.findAllByText('Password is required');
+
+    expect(container.innerHTML).toMatch('First name is required');
+    expect(container.innerHTML).toMatch('Last name is required');
+    expect(container.innerHTML).toMatch('Preferred Location is required');
+    expect(container.innerHTML).toMatch('Title is required');
+    expect(container.innerHTML).toMatch('Email is required');
+    expect(container.innerHTML).toMatch('User name is required');
+    expect(passwordError).toHaveLength(2);
+  });
+
+  it('should validate error for fields with invalid format', async () => {
+    const mockOnSubmit = jest.fn();
+
+    const { getByTestId, container } = render(
+      <GlobalProvider>
+        <Router>
+          <RegistrationForm onSubmit={mockOnSubmit} />
+        </Router>
+      </GlobalProvider>
+    );
+    await act(async () => {
+      fireEvent.input(getByTestId('firstName'), { target: { value: '1234' } });
+      fireEvent.input(getByTestId('lastName'), { target: { value: '1234' } });
+      fireEvent.input(getByTestId('prefLoc'), { target: { value: '11' } });
+      fireEvent.input(getByTestId('title'), {
+        target: { value: 'Software Engineer1' },
+      });
+      fireEvent.input(getByTestId('email'), {
+        target: { value: 'johndoe' },
+      });
+      fireEvent.input(getByTestId('userName'), {
+        target: { value: 'john.doe1@' },
+      });
+      fireEvent.input(getByTestId('password'), {
+        target: { value: 'a1234567' },
+      });
+      fireEvent.input(getByTestId('retypePassword'), {
+        target: { value: 'a1234567!' },
+      });
+    });
+
+    await act(async () => {
+      fireEvent.submit(getByTestId('submit'));
+    });
+
+    const onlyLetters = await screen.findAllByText(
+      'Must only contain letters (A-Z, a-z)'
+    );
+
+    expect(container.innerHTML).toMatch(
+      'Must only contain letters and spaces(A-Z, a-z)'
+    );
+    expect(container.innerHTML).toMatch('Invalid email format');
+    expect(container.innerHTML).toMatch(
+      'Only contains alphanumeric characters, underscore and dot'
+    );
+    expect(container.innerHTML).toMatch('Invalid password: (A-z 0-9 @$!%*?%)');
+    expect(onlyLetters).toHaveLength(3);
+  });
 });
--- src/components/AccountForms/RegistrationForm/index.js
+++ src/components/AccountForms/RegistrationForm/index.js
@@ -7,6 +7,9 @@ import { Typography, TextField, Button, Grid } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { GlobalContext } from 'context/global';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import isEmpty from 'lodash/isEmpty';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -103,15 +106,75 @@ const useStyles = makeStyles((theme) => ({
 
 const FORM_TITLE = 'Create account';
 const FORM_SUBTITLE = 'Have an existing account?';
-
+const MIN_CHARS = 'Must be more than 1 character';
+const ONLY_LETTERS = 'Must only contain letters (A-Z, a-z)';
+const ONLY_LETTERS_WS = 'Must only contain letters and spaces(A-Z, a-z)';
+const MIN_8CHARS = 'Must be more than 8 characters';
 function submitHandler(data) {
   alert(JSON.stringify(data));
 }
 
-const RegistrationForm = ({ onSubmit = submitHandler }) => {
+const RegistrationSchema = yup.object().shape({
+  firstName: yup
+    .string()
+    .required('First name is required')
+    .min(2, MIN_CHARS)
+    .matches(/^[a-zA-Z]+$/, ONLY_LETTERS),
+  lastName: yup
+    .string()
+    .required('Last name is required')
+    .min(2, MIN_CHARS)
+    .matches(/^[a-zA-Z]+$/, ONLY_LETTERS),
+  prefLoc: yup
+    .string()
+    .required('Preferred Location is required')
+    .min(2, MIN_CHARS)
+    .matches(/^[a-zA-Z]+$/, ONLY_LETTERS),
+  title: yup
+    .string()
+    .required('Title is required')
+    .min(2, MIN_CHARS)
+    .matches(/^[a-zA-Z\s]*$/, ONLY_LETTERS_WS),
+  email: yup
+    .string()
+    .required('Email is required')
+    .min(2, MIN_CHARS)
+    .matches(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Invalid email format'
+    ),
+  userName: yup
+    .string()
+    .required('User name is required')
+    .min(2, MIN_CHARS)
+    .matches(
+      /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/,
+      'Only contains alphanumeric characters, underscore and dot'
+    ),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, MIN_8CHARS)
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      'Invalid password: (A-z 0-9 @$!%*?%)'
+    ),
+  retypePassword: yup
+    .string()
+    .required('Password is required')
+    .min(8, MIN_8CHARS)
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      'Invalid password: (A-z 0-9 @$!%*?%)'
+    )
+    .oneOf([yup.ref('password'), null], "Passwords don't match."),
+});
+
+const RegistrationForm = ({ onSubmit = submitHandler, onError }) => {
   const classes = useStyles();
   const { register, handleSubmit, errors } = useForm({
     revalidateMode: 'onChange',
+    resolver: yupResolver(RegistrationSchema),
   });
   const {
     globalState: { isMobile },
@@ -130,71 +193,69 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                 <Typography variant="body2" className={classes.formSubtitle}>
                   {FORM_SUBTITLE} <Link to="/login">Login</Link>
                 </Typography>
-                <form noValidate onSubmit={handleSubmit(onSubmit)}>
+                <form noValidate onSubmit={handleSubmit(onSubmit, onError)}>
                   <div>
                     <TextField
                       {...{
+                        // 'data-testid': 'firstName',
                         label: 'First Name',
                         variant: 'outlined',
+                        inputProps: { 'data-testid': 'firstName' },
                         name: 'firstName',
-                        inputRef: register({ required: true }),
-                        error: errors.firstName,
+                        inputRef: register,
+                        error: !isEmpty(errors.firstName),
                         helperText:
-                          errors.firstName && 'First name is required',
+                          errors.firstName && errors.firstName.message,
                       }}
                     />
                     <TextField
                       {...{
-                        inputRef: register({ required: true }),
+                        inputProps: { 'data-testid': 'lastName' },
+                        inputRef: register,
                         name: 'lastName',
                         label: 'Last Name',
                         variant: 'outlined',
                         className: classes.textFieldSpacing,
-                        error: errors.lastName,
-                        helperText: errors.lastName && 'Last name is required',
+                        error: !isEmpty(errors.lastName),
+                        helperText: errors.lastName && errors.lastName.message,
                       }}
                     />
                   </div>
                   <div className={classes.inputSpacing}>
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'prefLoc' },
                         label: 'Preferred Location',
                         variant: 'outlined',
-                        inputRef: register({ required: true }),
-                        name: 'preferredLocation',
-                        error: errors.preferredLocation,
-                        helperText:
-                          errors.preferredLocation &&
-                          'Preferred Location is required',
+                        inputRef: register,
+                        name: 'prefLoc',
+                        error: !isEmpty(errors.prefLoc),
+                        helperText: errors.prefLoc && errors.prefLoc.message,
                       }}
                     />
                   </div>
                   <div className={classes.inputSpacing}>
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'title' },
                         label: 'Title',
                         variant: 'outlined',
-                        inputRef: register({ required: true }),
+                        inputRef: register,
                         name: 'title',
-                        error: errors.title,
-                        helperText: errors.title && 'Title is required',
+                        error: !isEmpty(errors.title),
+                        helperText: errors.title && errors.title.message,
                       }}
                     />
                   </div>
                   <div className={classes.inputSpacing}>
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'email' },
                         label: 'Email',
                         variant: 'outlined',
-                        inputRef: register({
-                          required: 'Email is required',
-                          pattern: {
-                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                            message: 'Invalid email format',
-                          },
-                        }),
+                        inputRef: register,
                         name: 'email',
-                        error: errors.email,
+                        error: !isEmpty(errors.email),
                         helperText: errors.email && errors.email.message,
                       }}
                     />
@@ -202,48 +263,39 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   <div className={classes.inputSpacing}>
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'userName' },
                         label: 'Username',
                         variant: 'outlined',
-                        inputRef: register({ required: true }),
+                        inputRef: register,
                         name: 'userName',
-                        error: errors.userName,
-                        helperText: errors.userName && 'Username is required',
+                        error: !isEmpty(errors.userName),
+                        helperText: errors.userName && errors.userName.message,
                       }}
                     />
                   </div>
                   <div className={classes.inputSpacing}>
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'password' },
                         label: 'Password',
                         variant: 'outlined',
                         name: 'password',
                         type: 'password',
-                        inputRef: register({
-                          required: 'Password is required',
-                          pattern: {
-                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                            message: 'Invalid password: (A-z@$!%*?%)',
-                          },
-                        }),
-                        error: errors.password,
+                        inputRef: register,
+                        error: !isEmpty(errors.password),
                         helperText: errors.password && errors.password.message,
                       }}
                     />
                     <TextField
                       {...{
+                        inputProps: { 'data-testid': 'retypePassword' },
                         label: 'Retype Password',
                         variant: 'outlined',
                         className: classes.textFieldSpacing,
                         name: 'retypePassword',
                         type: 'password',
-                        inputRef: register({
-                          required: 'Password is required',
-                          pattern: {
-                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                            message: 'Invalid password: (A-z@$!%*?%)',
-                          },
-                        }),
-                        error: errors.retypePassword,
+                        inputRef: register,
+                        error: !isEmpty(errors.retypePassword),
                         helperText:
                           errors.retypePassword &&
                           errors.retypePassword.message,
@@ -253,6 +305,7 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   <div>
                     <Button
                       {...{
+                        'data-testid': 'submit',
                         type: 'submit',
                         variant: 'contained',
                         color: 'primary',
@@ -327,9 +380,9 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   label: 'First Name',
                   variant: 'outlined',
                   name: 'firstName',
-                  inputRef: register({ required: true }),
+                  inputRef: register,
                   error: errors.firstName,
-                  helperText: errors.firstName && 'First name is required',
+                  helperText: errors.firstName && errors.firstName.message,
                   fullWidth: true,
                   className: classes.mobileInputField,
                 }}
@@ -339,9 +392,9 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   label: 'Last Name',
                   variant: 'outlined',
                   name: 'lastName',
-                  inputRef: register({ required: true }),
+                  inputRef: register,
                   error: errors.lastName,
-                  helperText: errors.lastName && 'Last name is required',
+                  helperText: errors.lastName && errors.lastName.message,
                   fullWidth: true,
                   className: classes.mobileInputField,
                 }}
@@ -351,10 +404,9 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   label: 'Preferred Location',
                   variant: 'outlined',
                   name: 'prefLoc',
-                  inputRef: register({ required: true }),
+                  inputRef: register,
                   error: errors.prefLoc,
-                  helperText:
-                    errors.prefLoc && 'Preferred Location is required',
+                  helperText: errors.prefLoc && errors.prefLoc.message,
                   fullWidth: true,
                   className: classes.mobileInputField,
                 }}
@@ -364,9 +416,9 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   label: 'Title',
                   variant: 'outlined',
                   name: 'title',
-                  inputRef: register({ required: true }),
+                  inputRef: register,
                   error: errors.title,
-                  helperText: errors.title && 'Title is required',
+                  helperText: errors.title && errors.title.message,
                   fullWidth: true,
                   className: classes.mobileInputField,
                 }}
@@ -375,13 +427,7 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                 {...{
                   label: 'Email',
                   variant: 'outlined',
-                  inputRef: register({
-                    required: 'Email is required',
-                    pattern: {
-                      value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                      message: 'Invalid email format',
-                    },
-                  }),
+                  inputRef: register,
                   name: 'email',
                   error: errors.email,
                   helperText: errors.email && errors.email.message,
@@ -394,9 +440,9 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   label: 'User name',
                   variant: 'outlined',
                   name: 'userName',
-                  inputRef: register({ required: true }),
+                  inputRef: register,
                   error: errors.userName,
-                  helperText: errors.userName && 'User name is required',
+                  helperText: errors.userName && errors.userName.message,
                   fullWidth: true,
                   className: classes.mobileInputField,
                 }}
@@ -409,13 +455,7 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   fullWidth: true,
                   name: 'password',
                   type: 'password',
-                  inputRef: register({
-                    required: 'Password is required',
-                    pattern: {
-                      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                      message: 'Invalid password: (A-z@$!%*?%)',
-                    },
-                  }),
+                  inputRef: register,
                   error: errors.password,
                   helperText: errors.password && errors.password.message,
                 }}
@@ -426,13 +466,7 @@ const RegistrationForm = ({ onSubmit = submitHandler }) => {
                   variant: 'outlined',
                   className: classes.mobileInputField,
                   fullWidth: true,
-                  inputRef: register({
-                    required: 'Password is required',
-                    pattern: {
-                      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                      message: 'Invalid password: (A-z@$!%*?%)',
-                    },
-                  }),
+                  inputRef: register,
                   error: errors.retypePassword,
                   helperText:
                     errors.retypePassword && errors.retypePassword.message,
