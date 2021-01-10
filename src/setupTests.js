827a0dfaaccf2cc455318dc7034f4542ad6ca05d
Robert Chen <robghchen@gmail.com>
2021-01-09 19:27:17 -0500

    fix(qa): temporarily disable mock service worker

--- src/setupTests.js
+++ src/setupTests.js
@@ -9,4 +9,4 @@ import '@testing-library/jest-dom/extend-expect';
 import MutationObserver from '@sheerun/mutationobserver-shim';
 window.MutationObserver = MutationObserver;
 
-import './testServer';
+// import './testServer';
