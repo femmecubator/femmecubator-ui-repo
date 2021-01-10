1a067bcc028a4d41b2d0a7e4bd26c9599f81d277
Carlo <ca.fernando415@gmail.com>
2021-01-10 08:23:35 -0500

    added eslintrule and removed unecessary code

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,8 +3,6 @@ import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import Header from 'components/Header/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { DEFAULT_COMMON_MENU } from 'utils/constants';
-import MobileHeader from './MobileHeader';
-import DesktopHeader from './DesktopHeader';
 
 jest.mock('react', () => ({
   ...jest.requireActual('react'),
