a06894adb9568638a5aa9520a7ab3e2086f25ddd
Carlo <ca.fernando415@gmail.com>
2021-02-09 00:27:28 -0500

    update wcag tools

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalProvider } from 'context/global';
+import axe from '@axe-core/react';
 
 if (process.env.NODE_ENV !== 'production') {
-  const axe = require('@axe-core/react');
   axe(React, ReactDOM, 1000);
 }
 
