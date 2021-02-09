83b2206a1b9a5ee2cf5f26be67cefd9bfbc1e4cd
Carlo <ca.fernando415@gmail.com>
2021-02-09 00:09:25 -0500

    update wcag tool

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalProvider } from 'context/global';
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(
   <GlobalProvider>
     <Router>
