e2c9a5ce57ac6268e7aa5df49cae4becef37c929
21shield <21netaly@gmail.com>
2021-02-16 23:14:59 -0500

    removed unused variable

---
+++ src/components/Subheader/Subheader.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import Subheader from './Subheader';
+import userEvent from '@testing-library/user-event';
+
+afterEach(cleanup);
+
+jest.mock('../../utils/auth');
+console.error = jest.fn();
+const properties = { variant: '', mainLabel: '' };
+const handleClick = jest.fn();
+
+describe('<Subheader/>', () => {
+  test('<Subheader/>', () => {
+    render(<Subheader {...properties} />);
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  test('Should Remove the Header', () => {
+    render(<Subheader {...properties} testClick={handleClick} />);
+    userEvent.click(screen.getByRole('button'));
+    expect(handleClick).toBeCalled();
+  });
+});
