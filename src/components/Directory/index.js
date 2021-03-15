328702182dd8a7cb3d44375189a5e1f8b7aea16e
Netaly <netaly.ramirez@gmail.com>
2021-03-15 19:43:18 -0400

    fixed axe wcag issues and moved subheader into directory

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Card from './MentorCard/card';
+import Subheader from '../Subheader/Subheader';
+import { ReactComponent as SubheaderIcon } from '../Subheader/assets/SubheaderIcon.svg';
 const index = () => {
+  const subheaderProperties = {
+    variant: 'mentors',
+    mainLabel: 'Connect with a Mentor',
+    subLabel: 'Book 30 mins with a UX Design or Coding Mentor.',
+    image: <SubheaderIcon />,
+  };
   return (
-    <main>
+    <section aria-label="Mentor Directory">
+      <Subheader {...subheaderProperties} />
       <div className="search">Search Bar</div>
       <div className="MentorCardContainter">
         <Card />
       </div>
-    </main>
+    </section>
   );
 };
 
