1e96a0a66e71ab7faa37c7633fc9ff6a63be81a8
Netaly <netaly.ramirez@gmail.com>
2021-03-29 13:24:53 -0400

    created story book component for Mentor Card

---
+++ src/stories/MentorCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MentorCard from '../components/Directory/MentorCard/MentorCard';
+
+export default {
+  title: 'Mentor Card',
+  component: MentorCard,
+};
+
+export const Template = (args) => <MentorCard {...args} />;
