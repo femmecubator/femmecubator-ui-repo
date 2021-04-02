560e7186765b90c13152c88771a2b47c8823652a
Netaly <netaly.ramirez@gmail.com>
2021-04-02 13:02:00 -0400

    Inluded mentor card component

--- src/components/Directory/MentorCard/MentorCard.test.js
+++ src/components/Directory/MentorCard/MentorCard.test.js
@@ -3,6 +3,7 @@ import { render, cleanup, screen } from '@testing-library/react';
 import MentorCard from './MentorCard';
 
 // describe accepts 2 arguments a string and a function
+afterEach(cleanup);
 describe('<MentorCard>', () => {
   render(<MentorCard {...properties} />);
 });
