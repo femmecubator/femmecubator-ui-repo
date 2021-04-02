c6849f12691fc6c2fe222e54cfa724cfc419b4cc
Netaly <netaly.ramirez@gmail.com>
2021-04-02 13:02:38 -0400

    properties to be included

--- src/components/Directory/MentorCard/MentorCard.test.js
+++ src/components/Directory/MentorCard/MentorCard.test.js
@@ -3,6 +3,7 @@ import { render, cleanup, screen } from '@testing-library/react';
 import MentorCard from './MentorCard';
 
 // describe accepts 2 arguments a string and a function
+// MentorCard should have a Mentors name/ icon/ and bio
 afterEach(cleanup);
 describe('<MentorCard>', () => {
   render(<MentorCard {...properties} />);
