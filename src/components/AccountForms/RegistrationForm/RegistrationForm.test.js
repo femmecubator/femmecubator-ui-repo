1c482d91dfa1d7e76f9ba5bbf57115afe4be6ea7
sherouk <sheroukomara@gmail.com>
2021-04-05 13:29:43 -0500

    added chip and form titles tests

--- src/components/AccountForms/RegistrationForm/RegistrationForm.test.js
+++ src/components/AccountForms/RegistrationForm/RegistrationForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, cleanup } from '@testing-library/react';
 import RegistrationForm from './index';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -19,7 +19,32 @@ describe('<RegistrationForm />', () => {
     );
   });
 
-  it.skip('should check if the submit button handler has been called', async () => {
+  it('should check that titles and mentor and mentee buttons render to page', async () => {
+    await act(async () => {
+      const formTitle = screen.getByText('Create account');
+      expect(formTitle).toBeInTheDocument();
+
+      const formSubtitle = screen.getByText('Have an existing account?');
+      expect(formSubtitle).toBeInTheDocument();
+
+      const loginLink = screen.getByText('Login');
+      expect(loginLink).toBeInTheDocument();
+      
+      //render(<Chip/>);
+      const chipSelect = screen.getByText('I want to sign up as a:')
+      expect(chipSelect).toBeInTheDocument();
+
+      const mentorSelect = screen.getByText('Mentor');
+      expect(mentorSelect).toBeInTheDocument();
+      fireEvent.click(mentorSelect);
+
+      const menteeSelect = screen.getByText('Mentee');
+      expect(menteeSelect).toBeInTheDocument();
+      fireEvent.click(menteeSelect);
+    })
+  });
+  
+/*   it.skip('should check if the submit button handler has been called', async () => {
     await act(async () => {
       fireEvent.input(screen.getByTestId('firstName'), {
         target: { value: 'John' },
@@ -27,18 +52,12 @@ describe('<RegistrationForm />', () => {
       fireEvent.input(screen.getByTestId('lastName'), {
         target: { value: 'Doe' },
       });
-      /* fireEvent.input(screen.getByTestId('prefLoc'), {
-        target: { value: 'NY' },
-      }); */
       fireEvent.input(screen.getByTestId('title'), {
         target: { value: 'Software Engineer' },
       });
       fireEvent.input(screen.getByTestId('email'), {
         target: { value: 'johndoe@gmail.com' },
       });
-      /* fireEvent.input(screen.getByTestId('userName'), {
-        target: { value: 'john.doe' },
-      }); */
       fireEvent.input(screen.getByTestId('password'), {
         target: { value: 'JDoe12345!' },
       });
@@ -51,9 +70,9 @@ describe('<RegistrationForm />', () => {
       fireEvent.submit(screen.getByTestId('submit'));
     });
     expect(mockOnSubmit).toHaveBeenCalled();
-  });
+  }); */
 
-  it.skip('should validate errors for required fields', async () => {
+  it('should validate errors for required fields', async () => {
     await act(async () => {
       fireEvent.submit(screen.getByTestId('submit'));
     });
@@ -61,22 +80,17 @@ describe('<RegistrationForm />', () => {
 
     screen.getByText(/first name is required/i);
     screen.getByText(/first name is required/i);
-    screen.getByText(/preferred location is required/i);
     screen.getByText(/title is required/i);
     screen.getByText(/email is required/i);
-    screen.getByText(/user name is required/i);
     expect(passwordError).toHaveLength(2);
   });
 
-  it.skip('should validate error for fields with invalid format', async () => {
+  it('should validate error for fields with invalid format', async () => {
     await act(async () => {
       fireEvent.input(screen.getByTestId('firstName'), {
         target: { value: '1234' },
       });
       fireEvent.input(screen.getByTestId('lastName'), {
-        target: { value: '1234' },
-      });
-      fireEvent.input(screen.getByTestId('prefLoc'), {
         target: { value: '1234' },
       });
       fireEvent.input(screen.getByTestId('title'), {
@@ -85,10 +99,6 @@ describe('<RegistrationForm />', () => {
       fireEvent.input(screen.getByTestId('email'), {
         target: { value: 'johndoe' },
       });
-      fireEvent.input(screen.getByTestId('userName'), {
-        target: { value: 'john.doe1@' },
-      });
-
       fireEvent.input(screen.getByTestId('password'), {
         target: { value: 'a1234567' },
       });
@@ -105,11 +115,8 @@ describe('<RegistrationForm />', () => {
       /must only contain letters/i
     );
     screen.getByText(/invalid email format/i);
-    screen.getByText(
-      /only contains alphanumeric characters, underscore and dot/i
-    );
     screen.getByText(/invalid password format/i);
     screen.getByText(/passwords do not match./i);
-    expect(onlyLetters).toHaveLength(4);
+    expect(onlyLetters).toHaveLength(3);
   });
 });
