80e233a194847dcd7aece5a7a44f54e4d004a5c5
Netaly <netaly.ramirez@gmail.com>
2021-04-13 01:24:32 -0400

    added tests functionality

--- src/components/Directory/Directory.test.js
+++ src/components/Directory/Directory.test.js
@@ -5,7 +5,13 @@ import Index from './index';
 
 beforeEach(() => render(<Index />));
 
-describe('<Index/>', () => {
+// tests must check if subheader component shows
+// tests must check on load that mentor data is filled
+// tests must check for search container
+// tests must ensure 'officeHours show up'
+// tests must ensure that Directory tab exsists
+
+describe('<Directory/>', () => {
   test('renders Director', () => {
     expect(1).toBeTruthy();
   });
