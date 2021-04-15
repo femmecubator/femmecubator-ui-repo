1944187aa1a30a920d11f40535b6c5bd94c883af
vuonga1103 <vuonga1103@yahoo.com>
2021-04-14 22:37:56 -0400

    clearned up AboutContainer.test.js

--- src/components/AboutContainer/AboutContainer.test.js
+++ src/components/AboutContainer/AboutContainer.test.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import AboutContainer from './AboutContainer';
-import { act } from 'react-dom/test-utils';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('<AboutContainer />', () => {
   beforeEach(() => {
-    act(() => {
-      render(
-        <Router>
-          <AboutContainer />
-        </Router>
-      );
-    });
+    render(
+      <Router>
+        <AboutContainer />
+      </Router>
+    );
   });
 
   const optionLabels = ['What We Do', 'Who We Are', 'Programs', 'Get Involved'];
@@ -28,15 +25,15 @@ describe('<AboutContainer />', () => {
 
   it('should fire a click event for all menu options', () => {
     optionLabels.forEach((optionLabel) => {
-      let optionLink = screen.getByTestId(optionLabel);
+      const optionLink = screen.getByTestId(optionLabel);
       fireEvent.click(optionLink);
     });
   });
 
-  it('should display corresponding title when an option is clicked', async () => {
-    await optionLabels.forEach((optionLabel) => {
-      let optionLink = screen.getByTestId(optionLabel);
-      let title = screen.getByTestId('title');
+  it('should display corresponding title when an option is clicked', () => {
+    optionLabels.forEach((optionLabel) => {
+      const optionLink = screen.getByTestId(optionLabel);
+      const title = screen.getByTestId('title');
       fireEvent.click(optionLink);
       expect(title).toHaveTextContent(optionLabel);
     });
