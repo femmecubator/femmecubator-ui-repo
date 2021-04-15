f184209a7390ce7cf8091085ab5f75d96eac687d
Netaly <netaly.ramirez@gmail.com>
2021-04-15 15:13:58 -0400

    updated import file for testing

--- src/components/Directory/MentorSearch/MentorSearch.test.js
+++ src/components/Directory/MentorSearch/MentorSearch.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import MentorSearchBar from './MentorSearchBar';
+import MentorSearchBar from './';
 
 const mockOnSubmit = jest.fn();
 beforeEach(() => render(<MentorSearchBar mockOnSubmit={mockOnSubmit} />));
