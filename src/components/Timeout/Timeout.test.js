bd4d57755eabc85f18ac01bf524574e12cd5b54d
sherouk <sheroukomara@gmail.com>
2021-04-19 16:20:26 -0500

    Update Timeout.test.js

--- src/components/Timeout/Timeout.test.js
+++ src/components/Timeout/Timeout.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Modal } from '../../stories/Timeout.stories';
-//import ModalPopup from './ModalPopup';
 
 it('renders the modal popup', () => {
     render(<Modal />);
