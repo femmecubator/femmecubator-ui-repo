7b5a36ca008fc71bc821c4df1a702d41d84f77d3
sherouk <sheroukomara@gmail.com>
2021-04-20 18:38:05 -0500

    wrote test for timeout function

--- src/components/Timeout/Timeout.test.js
+++ src/components/Timeout/Timeout.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Modal } from '../../stories/Timeout.stories';
+import { Timeout } from './Timeout';
+
+'use-strict'
+
+jest.useFakeTimers();
+const mockTimeout = jest.fn(Timeout);
+
+it('waits one second before showing the modal popup', () => {
+  //const mockTimeout = require('./Timeout');
+  expect(mockTimeout).not.toBeCalled();
+  jest.runAllTimers();
+  mockTimeout();
+  expect(mockTimeout).toHaveBeenCalled();
+  expect(mockTimeout).toHaveBeenCalledTimes(1);
+  expect(mockTimeout).toHaveBeenLastCalledWith();
+});
 
 it('renders the modal popup', () => {
-    render(<Modal />);
-    expect(screen.getByText('Your online session')).toBeInTheDocument();
-})
+  render(<Modal />);
+  expect(screen.getByText('Your online session')).toBeInTheDocument();
+});
