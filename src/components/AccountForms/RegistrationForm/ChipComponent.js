22d806c5e0c2fb75121d72e6b0ac9810b24f6b7a
cfernando415 <42846522+cfernando415@users.noreply.github.com>
2021-04-20 21:50:52 -0400

    fixed deployment issues (#124)

--- src/components/AccountForms/RegistrationForm/ChipComponent.js
+++ src/components/AccountForms/RegistrationForm/ChipComponent.js
@@ -7,7 +7,7 @@ import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 const ChipComponent = ({ register, unregister, watch, setValue, errors }) => {
   const classes = useStyles();
   const watchRole = watch('role_id', '');
-  const handleClick = (val) =>
+  const handleClick = val =>
     setValue('role_id', val, { shouldDirty: true, shouldValidate: true });
   useEffect(() => {
     register('role_id');
