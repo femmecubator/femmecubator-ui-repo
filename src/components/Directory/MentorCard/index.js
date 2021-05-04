7affec1b6732b8ae272d852d895aa8c4613ea456
Carlo <ca.fernando415@gmail.com>
2021-05-04 19:36:54 -0400

    removed leading space in MentorCard styles

--- src/components/Directory/MentorCard/index.js
+++ src/components/Directory/MentorCard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useStyles from './ MentorCard.styles';
+import useStyles from './MentorCard.styles';
 import {
   Card,
   CardHeader,
