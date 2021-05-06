e3e6013ec6fc798229fb3b92b6f4f530d0090eca
Eric SK.C <43048422+erocknine@users.noreply.github.com>
2021-05-06 18:54:42 -0400

    Feature/session timeout (#98)
    * Feature - Session Timeout
    
    * Added media query to useStyle for mobile size 600px
    
    * Removed CSS file
    
    * Added TrapFocus wrapper on Modal component
    
    * Refactored props, passed props to Modal, and onClick
    
    * merging from development
    
    * Update package.json
    
    * Update Timeout.js
    
    * separated modal component and created storybook
    
    * created separate stylesheet and adjusted mobile view
    
    still working on mobile styles
    
    * unit test created for modal render and text appearing
    
    need to write tests for setTimeout()
    
    * Update Timeout.test.js
    
    * wrote test for timeout function
    
    * Update Timeout.test.js
    
    * modal showing locally now
    
    * some cleanup
    
    * added a11y props
    
    * accessibility troubleshooting
    
    * Update ModalPopup.js
    
    * Update Timeout.test.js
    
    * Update Timeout.test.js
    
    * Update Timeout.test.js
    
    * skipped tests
    
    * Update ModalPopup.js
    
    * updating modal props
    
    * fixed most a11y issues, role name pending
    
    * Update Timeout.js
    
    * updated package.json
    
    * Update Timeout.test.js
    
    * added Button component
    
    * more accessibility changes
    
    * Update ModalPopup.js
    
    * Update Timeout.test.js
    
    * Update ModalPopup.js
    
    * changing role
    
    * skipping tests again
    
    * switched role for aria-live on modal
    
    * put children in div and set aria-hidden to true
    
    * tests are passing again
    
    * Update ModalPopup.js
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * final changes and code cleanup, ran npm fix for eslint error
    
    * Update package-lock.json
    
    * fixing package.json
    
    * resolving merge conflict
    
    * resolving merge conflict ii
    
    * Delete package-lock.json
    
    * updated props to ES6 syntax
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * update package.json
    
    Co-authored-by: cfernando415 <42846522+cfernando415@users.noreply.github.com>
    Co-authored-by: sherouk <sheroukomara@gmail.com>
    Co-authored-by: Sherouk O <73035812+sherouk@users.noreply.github.com>

---
+++ src/components/Timeout/ModalPopup.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import useStyles from './ModalPopup.styles';
+import FocusTrap from 'focus-trap-react';
+import Button from '@material-ui/core/Button';
+
+const ModalPopup = ({ countdownTime, setIdle, logoff, timedOut }) => {
+  const classes = useStyles();
+
+  const [minutes, setMinutes] = useState(Math.floor(countdownTime / 60000));
+  const [seconds, setSeconds] = useState((countdownTime / 1000) % 60);
+
+  useEffect(() => {
+    let time = countdownTime;
+
+    const timer =
+      countdownTime > 0 &&
+      setInterval(() => {
+        time -= 1000;
+        setMinutes(Math.floor(time / 60000));
+        setSeconds((time / 1000) % 60);
+        if (time === 0) {
+          time = 0;
+          clearInterval(timer);
+          timedOut();
+        }
+      }, 1000);
+
+    return () => clearInterval(timer);
+  }, [countdownTime, timedOut]);
+
+  return (
+    <div className={classes.modal_background}>
+      <FocusTrap open isEnabled={() => true} getDoc={() => document}>
+        <article className={classes.modal}>
+          <div className={classes.modal__center_div}>
+            <div className={classes.modal__center_div__content}>
+              <h1 className={classes.modal__center_div__content__p_head}>
+                Your online session
+              </h1>
+              <p className={classes.modal__center_div__content__p_head}>
+                will expire soon
+              </p>
+              <p className={classes.modal__center_div__content__p_timer}>
+                {`${minutes} min ${seconds < 10 ? '0' : ''}${seconds} seconds`}
+              </p>
+              <p className={classes.modal__center_div__content__p_text}>
+                Choose continue to keep working or log off.
+              </p>
+            </div>
+            <div className={classes.modal__center_div__buttons}>
+              <Button
+                {...{
+                  className: classes.modal__center_div__buttons__button_logoff,
+                  onClick: logoff,
+                  onKeyPress: logoff,
+                  ariaLabel: 'log off',
+                  tabIndex: 0,
+                }}
+              >
+                Log Off
+              </Button>
+              <Button
+                {...{
+                  className:
+                    classes.modal__center_div__buttons__button_continue,
+                  onClick: () => setIdle(false),
+                  onKeyPress: () => setIdle(false),
+                  ariaLabel: 'continue',
+                  tabIndex: 0,
+                }}
+              >
+                Continue
+              </Button>
+            </div>
+          </div>
+        </article>
+      </FocusTrap>
+    </div>
+  );
+};
+
+export default ModalPopup;
