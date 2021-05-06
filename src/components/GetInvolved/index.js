ca52c8e09b35ad05b0d3564f445f3d04e828991f
sherouk <sheroukomara@gmail.com>
2021-05-06 12:53:34 -0500

    files created

---
+++ src/components/GetInvolved/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+function GetInvolved() {
+  return <div>this is a page</div>;
+}
+
+export default GetInvolved;
