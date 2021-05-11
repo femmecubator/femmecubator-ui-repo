e9de55c6c70abe916d8d38a75b06504843f1888f
sherouk <sheroukomara@gmail.com>
2021-05-11 12:44:58 -0500

    ready for PR

--- src/components/AboutContainer/AboutContentContainer/AboutContentPages/GetInvolved.js
+++ src/components/AboutContainer/AboutContentContainer/AboutContentPages/GetInvolved.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-//import Divider from '@material-ui/core/Divider';
 import useStyles from '../AboutContentContainer.styles';
 import { withStyles } from '@material-ui/core/styles';
 
