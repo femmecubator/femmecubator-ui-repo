9610c9ea7990b93c966196169a0fb17d44873933
PeaWarrior <jacksonchen171@gmail.com>
2021-05-13 16:57:22 -0400

    add mui to timeout modal

--- src/components/Timeout/ModalPopup.js
+++ src/components/Timeout/ModalPopup.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useStyles from './ModalPopup.styles';
-import FocusTrap from 'focus-trap-react';
-import Button from '@material-ui/core/Button';
+import FocusTrapOverlay from '../FocusTrapOverlay';
+import { Paper, Typography, Button } from '@material-ui/core';
 
 const ModalPopup = ({ countdownTime, setIdle, logoff, timedOut }) => {
   const classes = useStyles();
@@ -29,54 +29,72 @@ const ModalPopup = ({ countdownTime, setIdle, logoff, timedOut }) => {
   }, [countdownTime, timedOut]);
 
   return (
-    <div className={classes.modal_background}>
-      <FocusTrap open isEnabled={() => true} getDoc={() => document}>
-        <article className={classes.modal}>
-          <div className={classes.modal__center_div}>
-            <div className={classes.modal__center_div__content}>
-              <h1 className={classes.modal__center_div__content__p_head}>
-                Your online session
-              </h1>
-              <p className={classes.modal__center_div__content__p_head}>
-                will expire soon
-              </p>
-              <p className={classes.modal__center_div__content__p_timer}>
-                {`${minutes} min ${seconds < 10 ? '0' : ''}${seconds} seconds`}
-              </p>
-              <p className={classes.modal__center_div__content__p_text}>
-                Choose continue to keep working or log off.
-              </p>
-            </div>
-            <div className={classes.modal__center_div__buttons}>
-              <Button
-                {...{
-                  className: classes.modal__center_div__buttons__button_logoff,
-                  onClick: logoff,
-                  onKeyPress: logoff,
-                  'aria-label': 'log off',
-                  tabIndex: 0,
-                }}
-              >
-                Log Off
-              </Button>
-              <Button
-                {...{
-                  className:
-                    classes.modal__center_div__buttons__button_continue,
-                  onClick: () => setIdle(false),
-                  onKeyPress: () => setIdle(false),
-                  'aria-label': 'continue',
-                  tabIndex: 0,
-                }}
-              >
-                Continue
-              </Button>
-            </div>
-          </div>
-        </article>
-      </FocusTrap>
-    </div>
+    <FocusTrapOverlay open={open}>
+      <Paper>
+        <Typography variant="h4" component="h1" align="center">
+          Your online session will expire soon
+        </Typography>
+        <Typography variant="h5" component="p" align="center">
+          {`${minutes} min ${seconds < 10 ? '0' : ''}${seconds} seconds`}
+        </Typography>
+        <Typography variant="body1" component="p" align="center">
+          Choose continue to keep working or log off.
+        </Typography>
+        <Button>Log Off</Button>
+        <Button>Continue</Button>
+      </Paper>
+    </FocusTrapOverlay>
   );
+
+  // return (
+  //   <div className={classes.modal_background}>
+  //     <FocusTrap open isEnabled={() => true} getDoc={() => document}>
+  //       <article className={classes.modal}>
+  //         <div className={classes.modal__center_div}>
+  //           <div className={classes.modal__center_div__content}>
+  //             <h1 className={classes.modal__center_div__content__p_head}>
+  //               Your online session
+  //             </h1>
+  //             <p className={classes.modal__center_div__content__p_head}>
+  //               will expire soon
+  //             </p>
+  //             <p className={classes.modal__center_div__content__p_timer}>
+  //               {`${minutes} min ${seconds < 10 ? '0' : ''}${seconds} seconds`}
+  //             </p>
+  //             <p className={classes.modal__center_div__content__p_text}>
+  //               Choose continue to keep working or log off.
+  //             </p>
+  //           </div>
+  //           <div className={classes.modal__center_div__buttons}>
+  //             <Button
+  //               {...{
+  //                 className: classes.modal__center_div__buttons__button_logoff,
+  //                 onClick: logoff,
+  //                 onKeyPress: logoff,
+  //                 'aria-label': 'log off',
+  //                 tabIndex: 0,
+  //               }}
+  //             >
+  //               Log Off
+  //             </Button>
+  //             <Button
+  //               {...{
+  //                 className:
+  //                   classes.modal__center_div__buttons__button_continue,
+  //                 onClick: () => setIdle(false),
+  //                 onKeyPress: () => setIdle(false),
+  //                 'aria-label': 'continue',
+  //                 tabIndex: 0,
+  //               }}
+  //             >
+  //               Continue
+  //             </Button>
+  //           </div>
+  //         </div>
+  //       </article>
+  //     </FocusTrap>
+  //   </div>
+  // );
 };
 
 export default ModalPopup;
