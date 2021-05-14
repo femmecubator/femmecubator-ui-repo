5878258e5a07bf3de3fd5621d80c09cdbbc0253e
PeaWarrior <jacksonchen171@gmail.com>
2021-05-14 12:05:39 -0400

    fix css to work on safari

--- src/components/FocusTrapOverlay/FocusTrapOverlay.styles.js
+++ src/components/FocusTrapOverlay/FocusTrapOverlay.styles.js
@@ -3,9 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   root: {
     position: 'fixed',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
     padding: '10px',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    inset: 0,
     display: 'block',
     overflow: 'scroll',
     zIndex: 1300,
