abd6c26b39803c08448e8836291ed80013015e16
sherouk <sheroukomara@gmail.com>
2021-06-08 21:38:57 -0500

    format adjustments and working on mobile view fix

--- src/components/AboutContainer/AboutContentContainer/AboutContentPages/GetInvolved.js
+++ src/components/AboutContainer/AboutContentContainer/AboutContentPages/GetInvolved.js
@@ -20,11 +20,11 @@ const BlueOutlineButton = withStyles({
       border: 'none',
       color: 'white',
     },
-    '@media (max-width: 799px)': {
+    /* '@media (max-width: 799px)': {
       alignSelf: 'center',
       marginTop: 'auto',
       marginBottom: '20px',
-    },
+    }, */
   },
   label: {
     fontFamily: 'Open Sans, sans-serif',
@@ -50,28 +50,36 @@ const GetInvolved = () => {
         }}
       >
         <Grid item xs>
-          <h3>Donate</h3>
-          <p>
-            We are fundraising for our first mentorship program cohort which
-            will launch by March. Our goal is to mentor 10 women of color in
-            tech by 2021. Join us by supporting this program and create a pledge
-            on Patreon.
-          </p>
-          <BlueOutlineButton variant="outlined" data-testid="donateButton">
-            MAKE A DONATION
-          </BlueOutlineButton>
+          <item>
+            <h3>Donate</h3>
+            <p>
+              We are fundraising for our first mentorship program cohort which
+              will launch by March. Our goal is to mentor 10 women of color in
+              tech by 2021. Join us by supporting this program and create a
+              pledge on Patreon.
+            </p>
+          </item>
+          <item>
+            <BlueOutlineButton variant="outlined" data-testid="donateButton">
+              MAKE A DONATION
+            </BlueOutlineButton>
+          </item>
         </Grid>
         <Grid item xs>
-          <h3>Grow with us</h3>
-          <p>
-            We're sharing updates on our app-building process on Patreon every
-            month. Let's grow together. Take a peek at how we are building the
-            community platform and support our volunteers with a recurring gift
-            while you learn MongoDB, Express, React.js, and Node.js.
-          </p>
-          <BlueOutlineButton variant="outlined" data-testid="patreonButton">
-            SUPPORT ON PATREON
-          </BlueOutlineButton>
+          <item>
+            <h3>Grow with us</h3>
+            <p>
+              We're sharing updates on our app-building process on Patreon every
+              month. Let's grow together. Take a peek at how we are building the
+              community platform and support our volunteers with a recurring
+              gift while you learn MongoDB, Express, React.js, and Node.js.
+            </p>
+          </item>
+          <item>
+            <BlueOutlineButton variant="outlined" data-testid="patreonButton">
+              SUPPORT ON PATREON
+            </BlueOutlineButton>
+          </item>
         </Grid>
       </Grid>
       <br />
@@ -84,29 +92,37 @@ const GetInvolved = () => {
         }}
       >
         <Grid item xs={7}>
-          <h3>Mentor</h3>
-          <p>
-            Our members need you! If you are an industry expert in Design or
-            Development and can allott an hour a week for career support,
-            ranging from tech interviews and whiteboarding sessions to
-            informational interviews, consider applying to be a mentor.
-          </p>
-          <BlueOutlineButton variant="outlined" data-testid="mentorButton">
-            SIGN UP TO BE A MENTOR
-          </BlueOutlineButton>
+          <item>
+            <h3>Mentor</h3>
+            <p>
+              Our members need you! If you are an industry expert in Design or
+              Development and can allott an hour a week for career support,
+              ranging from tech interviews and whiteboarding sessions to
+              informational interviews, consider applying to be a mentor.
+            </p>
+          </item>
+          <item true>
+            <BlueOutlineButton variant="outlined" data-testid="mentorButton">
+              BECOME A MENTOR
+            </BlueOutlineButton>
+          </item>
         </Grid>
         <br />
         <Grid item xs={7}>
-          <h3>Volunteer</h3>
-          <p>
-            Are you a UX or Web Dev bootcamp grad looking to expand your skills?
-            Have at least 5 to 7 hours a week to devote to learning what it's
-            like to be on a team? Be part of our agile team and dive into a
-            3-month AppDev Program.
-          </p>
-          <BlueOutlineButton variant="outlined" data-testid="volunteerButton">
-            APPLY TO VOLUNTEER
-          </BlueOutlineButton>
+          <item>
+            <h3>Volunteer</h3>
+            <p>
+              Are you a UX or Web Dev bootcamp grad looking to expand your
+              skills? Have at least 5 to 7 hours a week to devote to learning
+              what it's like to be on a team? Be part of our agile team and dive
+              into a 3-month AppDev Program.
+            </p>
+          </item>
+          <item>
+            <BlueOutlineButton variant="outlined" data-testid="volunteerButton">
+              APPLY TO VOLUNTEER
+            </BlueOutlineButton>
+          </item>
         </Grid>
       </Grid>
     </>
